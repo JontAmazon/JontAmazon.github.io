d0466b446777eff11def105d89a137896a6b1e9f
JontAmazon <jonatan.lindholm10@gmail.com>
2025-10-20 13:57:18 +0200

    Split projects into '2025' and 'Previous'

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   name: "Jonatan Lindholm",
-  title: "Software Engineer",
+  title: "Software Engineer/Tester",
   description: "Simple portfolio website of Jonatan Lindholm",
   accentColor: "#1d4ed8",
   social: {
@@ -18,20 +18,17 @@ export const siteConfig = {
     "Based in Lund, Sweden. Currently exploring new projects and opportunities."
   ],
   skills: [],
-  projects: [
+  projectHeaders: [
+    "Summer/Fall 2025",
+    "Fun Previous Projects",
+  ],
+  projects_2025: [
     {
       name: "WhatsApp Weather Bot",
       description:
       "A WhatsApp weather bot that sends daily weather updates using Twilio.",
       link: "https://weather-whatsapp-bot.fly.dev/",
       skills: ["Python", "Docker", "Fly.io", "REST API", "Database", "GH Actions", "Scheduling"],
-    },
-    {
-      name: "Acceleration Ball",
-      description:
-      "A fast-paced 2D game where you collect apples while avoiding monsters and emerging magma.",
-      link: "/acceleration-ball",
-      skills: ["Java", "OOP", "Game Loop & Animation"],
     },
     {
       name: "Playwright Practice",
@@ -41,24 +38,33 @@ export const siteConfig = {
       skills: ["JavaScript", "Playwright", "Automated Web Testing"],
     },
     {
-      name: "Yatzy Simulation",
-      description: "Heuristic-based algorithm for playing Yatzy.",
-      link: "https://yatzy-solver.fly.dev/",
-      skills: ["Python"],
-    },
-    {
       name: "Apple Dash",
       description:
       "Simple web game with a Node.js backend and SQLite highscore database.",
       link: "https://apple-dash.fly.dev/",
       skills: ["JavaScript", "Node.js", "Express", "REST API", "Database"],
     },
+  ],
+  previous_projects: [
+    {
+      name: "Acceleration Ball",
+      description:
+      "A fast-paced 2D game where you collect apples while avoiding monsters and emerging magma.",
+      link: "/acceleration-ball",
+      skills: ["Java", "OOP", "Game Loop & Animation"],
+    },
+    {
+      name: "Yatzy Simulation",
+      description: "Heuristic-based algorithm for playing Yatzy.",
+      link: "https://yatzy-solver.fly.dev/",
+      skills: ["Python"],
+    },
     {
       name: "Master's Thesis Popular Summary",
       description:
         "Deep learning models for clustering and classifying radar data.",
       link: "/masters-thesis",
-      skills: ["Python", "TensorFlow", "Deep Learning"],
+      skills: ["Python", "Deep Learning", "TensorFlow"],
     },
   ],
   // memes: [] ... maybe? ;)
